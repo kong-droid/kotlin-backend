7cca323b8cc3a4122affdf814e41b56abd5532b8
kong-droid <rhdalgid134@gmail.com>
2023-11-08 17:59:08 +0900

    fix folder structure

--- src/main/kotlin/coffee/kotlin/backend/KotlinBackendApplication.kt
+++ src/main/kotlin/coffee/kotlin/backend/KotlinBackendApplication.kt
@@ -1,4 +1,4 @@
-package site.kongdroid.kotlinbackend
+package coffee.kotlin.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
