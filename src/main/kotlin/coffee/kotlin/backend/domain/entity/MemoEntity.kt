0c0b5196f973ef0890c1f3dc4574b77d5de2061a
kong-driod <rhdalgid134@gmail.com>
2023-12-14 00:19:52 +0900

    fix entity.

--- src/main/kotlin/coffee/kotlin/backend/domain/entity/MemoEntity.kt
+++ src/main/kotlin/coffee/kotlin/backend/domain/entity/MemoEntity.kt
@@ -1,21 +1,26 @@
 package coffee.kotlin.backend.domain.entity
-
-import coffee.kotlin.backend.domain.entity.common.ImmutableUuidEntity
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.GenericGenerator
 import java.time.Instant
 import java.util.UUID
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "memo")
 data class MemoEntity (
     @Id
-    override val id: UUID = UUID.randomUUID(),
+    @Column(updatable = false, columnDefinition = "uuid")
+    @GenericGenerator(name = "system", strategy = "uuid2")
+    @GeneratedValue(generator = "system")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    val id: UUID = UUID.randomUUID(),
     @Column(unique = true)
     val name: String,
     val password: String,
     val contents: String,
-    override val createdAt: Instant
-): ImmutableUuidEntity(createdAt, id)
+    @Column(updatable = false)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @CreationTimestamp
+    val createdAt: Instant = Instant.now()
+)
